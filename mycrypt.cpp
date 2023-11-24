6ba76c714f45ef0398e41bd18ce19abce7211980
how2051 <chenhao_public@163.com>
2023-11-24 11:10:16 +0800

    [feat] index duplicated file name
    this is my first time to write code that vary from platform,
    first time to use platform marco
    
    1.when file name duplicated, add index postfix
    2.change end-of-line sequence from CRLF to LF

--- mycrypt.cpp
+++ mycrypt.cpp
@@ -3,6 +3,12 @@
 #include <string>
 #include <algorithm>
 
+#ifdef _WIN32
+#include <io.h>
+#elif __linux__
+#include <unistd.h>
+#endif
+
 enum DIRECTION {
 	ENCRYPT = 0,
 	DECRYPT = 1,
@@ -64,9 +70,24 @@ std::string getFilename(const std::string inputFile) {
     if (lastSeparatorPos != 0) {
         fileName = inputFile.substr(lastSeparatorPos + 1);
         std::cout << "File Name: " << fileName << std::endl;
-		std::replace(fileName.begin(), fileName.end(), '.', '_');
     }
+	std::replace(fileName.begin(), fileName.end(), '.', '_');
 	return fileName;
+}
+
+//find a new suitable filename if current name is occupied
+std::string findAvailableFileName(const std::string& baseName) {
+    std::string fileName = baseName + ".bin";
+
+    int index = 1;
+#ifdef _WIN32
+    while (_access(fileName.c_str(), 0) == 0) {
+#elif __linux__
+	while (access(fileName.c_str(), F_OK) == 0) {
+#endif
+        fileName = baseName + std::to_string(index) + ".bin";
+        index++;
+    } return fileName;
 }
 
 
@@ -74,7 +95,7 @@ int main(int argc, char* argv[]) {
 	if(argc < 2) { return -1; }
 	//@TODO: add logic to parse input argv, to use multi flags
     std::string inputFile = argv[1];
-    std::string encryptedFile = getFilename(inputFile) + ".bin";	//@TODO: generate multi index file name
+    std::string encryptedFile = findAvailableFileName(getFilename(inputFile));
 	std::cout << "Encrypted: " << encryptedFile << std::endl;
     EncryptDecrypt(inputFile, encryptedFile, ENCRYPT);
     std::cout << "File encrypted successfully." << std::endl;
