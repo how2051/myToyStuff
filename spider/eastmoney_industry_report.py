d0130261cc282a018d2e82a570e43658d858ca08
how2051 <chenhao_public@163.com>
2024-06-20 16:55:11 +0800

    update spider for eastmoney

--- spider/eastmoney_industry_report.py
+++ spider/eastmoney_industry_report.py
@@ -5,6 +5,17 @@ from bs4 import BeautifulSoup
 import os
 
 
+
+
+report_url_prefix = "https://reportapi.eastmoney.com/report/list?"
+
+headers = {
+	"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0"
+}
+
+
+
+
 def getPdfLink(infoCode):
 	report_url = "https://data.eastmoney.com/report/zw_industry.jshtml?infocode=" + infoCode
 	response = requests.get(report_url)
@@ -27,7 +38,11 @@ def getPdfLink(infoCode):
 
 
 
+
 def downloadPdf(url, filename):
+    if '/' in filename:
+        print(f"skip {filename}")
+        return
     folder = "report"
     if not os.path.exists(folder):
         os.makedirs(folder)
@@ -44,31 +59,35 @@ def downloadPdf(url, filename):
 
 
 
-url = "https://reportapi.eastmoney.com/report/list?industryCode=*&pageSize=50&industry=*&rating=*&ratingChange=*&beginTime=2022-06-20&endTime=2024-06-20&pageNo=1&fields=&qType=1&orgCode=&rcode=&p=3&pageNum=3&pageNumber=1"
 
-headers = {
-	"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0"
-}
+def fetchNPageReport(pageNumber):
+    for page in range(1, pageNumber+1):
+        report_url_postfix = f"industryCode=*&pageSize=50&industry=*&beginTime=*&endTime=*&pageNo={page}&qType=1"
+        url = report_url_prefix + report_url_postfix
+        response = requests.get(url=url, headers=headers)
+        results = response.json()
+        # print(results['data'])
+    
+        for item in results['data']:
+            print(item['industryName'])
+            print(f"《{item['title']}》by {item['orgSName']} : {item['researcher']}")
+            # print(item['orgName'])
+            # print(item['orgSName'])
+            # print(item['infoCode'])
+            # print(item['publishDate'])
+            # print(item['author'])
+            # print(item['researcher'])
+            # print(item['attachSize'])
+            # print(item['count'])
+            pdfLink = getPdfLink(item['infoCode'])
+            print(pdfLink)
+            downloadPdf(pdfLink, f"{item['title']}.pdf")
+            print('\n')
 
-for page_index in range(0, 100, 50):
-    response = requests.get(url=url, headers=headers)
-    results = response.json()
-    # print(results['data'])
-
-for item in results['data']:
-    print(item['industryName'])
-    print(f"《{item['title']}》by {item['orgSName']} : {item['researcher']}")
-    # print(item['orgName'])
-    # print(item['orgSName'])
-    # print(item['infoCode'])
-    # print(item['publishDate'])
-    # print(item['author'])
-    # print(item['researcher'])
-    pdfLink = getPdfLink(item['infoCode'])
-    downloadPdf(pdfLink, f"{item['title']}.pdf")
-    # print(pdfLink)
-    print('\n')
 
 
+
+
+fetchNPageReport(3)
 
 
