ac1de81462d0800f3e07f95b2d855165970012a4
how2051 <chenhao_public@163.com>
2024-06-20 17:21:42 +0800

    for eastmoney spider, add param to filter industry

--- spider/eastmoney_industry_report.py
+++ spider/eastmoney_industry_report.py
@@ -13,6 +13,21 @@ headers = {
 	"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0"
 }
 
+industryNameSet = {
+'',
+'美容护理','医疗服务','玻璃玻纤','通信设备','商业百货','装修建材','汽车整车',
+'仪器仪表','电子元件','农牧饲渔','文化传媒','光伏设备','家用轻工','计算机设备',
+'软件开发','有色金属','非金属材料','电力行业','生物制品','互联网服务','能源金属',
+'纺织服装','环保行业','船舶制造','半导体','钢铁行业','化肥行业','贵金属',
+'电网设备','家电行业','通用设备','铁路公路','化学制品','专用设备','水泥建材',
+'汽车零部件','石油行业','旅游酒店','航空机场','航天航空','装修装饰','汽车服务',
+'化学制药','食品饮料','交运设备','工程机械','专业服务','电池','煤炭行业',
+'公用事业','航运港口','教育','房地产服务','风电设备','塑料制品','化纤行业',
+'中药','造纸印刷','医疗器械','化学原料','游戏','通信服务','房地产开发','工程建设',
+'银行','证券','小金属','橡胶制品','光学光电子','燃气','保险','消费电子',
+'电源设备','包装材料','医药商业','酿酒行业','物流行业','多元金融'
+}
+
 
 
 
@@ -60,7 +75,9 @@ def downloadPdf(url, filename):
 
 
 
-def fetchNPageReport(pageNumber):
+def fetchNPageReport(pageNumber, industry=''):
+    if industry not in industryNameSet:
+        return
     for page in range(1, pageNumber+1):
         report_url_postfix = f"industryCode=*&pageSize=50&industry=*&beginTime=*&endTime=*&pageNo={page}&qType=1"
         url = report_url_prefix + report_url_postfix
@@ -69,6 +86,8 @@ def fetchNPageReport(pageNumber):
         # print(results['data'])
     
         for item in results['data']:
+            if(industry != '' and industry != item['industryName']):
+                continue
             print(item['industryName'])
             print(f"《{item['title']}》by {item['orgSName']} : {item['researcher']}")
             # print(item['orgName'])
